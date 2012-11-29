d0dbd6a70a38a08d01b6620ee42b0bebd65c8c06
Rolando Espinoza La fuente <darkrho@gmail.com>
2012-11-29 13:22:20 -0400

    fixed missing template_tags package.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "django-dummyimage",
@@ -9,7 +9,7 @@ setup(
     author_email = "darkrho@gmail.com",
     url = "https://github.com/darkrho/django-dummyimage",
     license = "BSD",
-    packages = ['dummyimage'],
+    packages = find_packages(),
     zip_safe=False, # because we're including media that Django needs
     include_package_data = True,
     install_requires = [
